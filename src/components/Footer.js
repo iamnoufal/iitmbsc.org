b7515703d3af33b6ef0d74d6ca342697e3c8ff4c
Ravi Kumawat <49914487+ravikumawat7716@users.noreply.github.com>
2022-02-16 20:47:28 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-color-gold px-lg-5 py-4 font-righteous">
       <div className="mx-lg-5 px-lg-5">
-        <p className="mx-5 m-0 fs-6">IIT Madras BSc Students is a student organization under IIT Madras under the BSc Degree Program - learn more at https://onlinedegree.iitm.ac.in<br />If you would like to rock some cool merchandise, check out : podgoodies.onlinedegree.iitm.ac.in</p>
+        <p className="mx-5 m-0 fs-6">IIT Madras BSc Students is a student organization under IIT Madras under the BSc Degree Program - learn more at <a href="https://onlinedegree.iitm.ac.in" target={"_blank"}>https://onlinedegree.iitm.ac.in</a><br />If you would like to rock some cool merchandise, check out : <a href="podgoodies.onlinedegree.iitm.ac.in" target={"_blank"}>podgoodies.onlinedegree.iitm.ac.in</a></p>
       </div>
     </footer>
   );
